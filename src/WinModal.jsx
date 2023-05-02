9b8f729714d0230cf0a3aa642c8923746c7eb641
derickcoleman87 <107764740+derickcoleman87@users.noreply.github.com>
2023-05-01 20:39:26 -0400

    combined app and game files into app.

--- src/WinModal.jsx
+++ src/WinModal.jsx
@@ -1,14 +1,31 @@
 import React from "react";
 import "./WinModal.css";
 
-function WinModal({ winner, roundWinner }) {
+function WinModal({
+  winner,
+  roundWinner,
+  refreshBoard,
+  setXWins,
+  setOWins,
+  setTies,
+}) {
+  function quit() {
+    refreshBoard();
+    setXWins(0);
+    setOWins(0);
+    setTies(0);
+  }
   return (
     <div className="win-modal">
       <p className="gray-text">{roundWinner}</p>
       <h1 className="turquoise-text">{winner} </h1>
       <div className="win-modal_actions">
-        <button className="quit-btn">Quit</button>
-        <button className="next-round">Next Round</button>
+        <button className="quit-btn" onClick={quit}>
+          Quit
+        </button>
+        <button className="next-round" onClick={refreshBoard}>
+          Next Round
+        </button>
       </div>
     </div>
   );
